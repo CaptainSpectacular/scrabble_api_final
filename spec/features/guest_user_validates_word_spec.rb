5d69aec84c5ea77035b6f240ea3a8d84f75d4424
pollockcl <pollock.clee@gmail.com>
2018-05-30 09:07:00 -0600

    first feature test

---
+++ spec/features/guest_user_validates_word_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'guest user validates word' do
+
+
+  describe 'searching for a word' do
+
+  background do 
+    visit root_path
+    fill_in('q'), with: 'foxes'
+    click_on 'Validate Word'
+  end
+
+    scenario 'the user sees whether it is a word or not' do 
+      expect(page).to have_content("'foxes' is a valid word and its root form is 'fox'")
+    end
+  end
+end
